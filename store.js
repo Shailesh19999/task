702b3c1284bce5404bba2c57f25361a69020eca6
Shailesh19999 <105121616+Shailesh19999@users.noreply.github.com>
2022-07-02 10:10:58 +0530

    Delete store.js

--- store.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from "redux";
-// import { configureStore } from '@reduxjs/toolkit'
-import rootred from "./reducer/main";
-const store= createStore(rootred);
-export default store;
